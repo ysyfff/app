e7d1cceae035f0c7767163693e5f04983a2eb653
ysy <1156546473@qq.com>
2016-12-16 09:44:36 +0800

    use ma

--- src/kit/memo/Memo.js
+++ src/kit/memo/Memo.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
-import {Text, View, StyleSheet, TouchableOpacity, TextInput, ListView} from 'react-native'
+import {Text, View, StyleSheet, TouchableOpacity, TextInput, ListView, AsyncStorage} from 'react-native'
 import ViewContainer from '../../common/ViewContainer'
 import Icon from 'react-native-vector-icons/FontAwesome'
-
+import _ from 'lodash'
 import Skin from '../../common/Skin'
 
 class Enter extends Component {
@@ -46,7 +46,7 @@ class Undo extends Component {
         super(props)
         this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
-            dataSource: this.ds.cloneWithRows([{text: 1}])
+            dataSource: this.ds.cloneWithRows([])
         }
     }
 
@@ -60,35 +60,63 @@ class Undo extends Component {
                     </Text>
                 </View>
                 <View>
-                    <ListView
-                        enableEmptySections={true}
-                        keyboardShouldPersistTaps={true}
-                        dataSource={this.ds.cloneWithRows(this.props.dataSource)}
-                        renderRow={(row, aa, index)=>{
-                            return (
-                                <View style={undoStyle.row}>
-                                    <View style={undoStyle.textContainer}>
-                                        <Text>
-                                            {row.text}
-                                        </Text>
-                                    </View>
-                                    <View style={undoStyle.btnContainer}>
-                                        <TouchableOpacity style={undoStyle.btn} onPress={() => {
-                                                this.props.onFinish && this.props.onFinish(index);
-                                            }}>
-                                            <Icon name="close" size={18} color={Skin.baseColor}/>
-                                        </TouchableOpacity>
-
-                                        <TouchableOpacity style={undoStyle.btn} onPress={() => {
-                                                this.props.onDelete && this.props.onDelete(index);
-                                            }}>
-                                            <Icon name="check" size={18} color={Skin.baseColor}/>
-                                        </TouchableOpacity>
-                                    </View>
+                    {this.props.dataSource.map((row, index)=>{
+                        return (
+                            <View style={undoStyle.row} key={index}>
+                                <View style={undoStyle.textContainer}>
+                                    <Text>
+                                        {row.text}
+                                    </Text>
                                 </View>
-                            )
-                        }}
-                    />
+
+                                <View style={undoStyle.btnContainer}>
+                                    <TouchableOpacity style={undoStyle.btn} onPress={() => {
+                                            this.props.onDelete && this.props.onDelete(index);
+                                        }}>
+                                        <Icon name="close" size={18} color={Skin.baseColor}/>
+                                    </TouchableOpacity>
+
+                                    <TouchableOpacity style={undoStyle.btn} onPress={() => {
+                                            this.props.onFinish && this.props.onFinish(index);
+                                        }}>
+                                        <Icon name="check" size={18} color={Skin.baseColor}/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        )
+                    })}
+
+                </View>
+            </View>
+        )
+    }
+}
+
+class Done extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <View style={undoStyle.container}>
+                <View>
+                    <Text>
+                        已办事项
+                    </Text>
+                </View>
+                <View>
+                    {this.props.dataSource.map((row, index)=>{
+                        return (
+                            <View style={undoStyle.row} key={index}>
+                                <View style={undoStyle.textContainer}>
+                                    <Text>
+                                        {row.text}
+                                    </Text>
+                                </View>
+                            </View>
+                        )
+                    })}
                 </View>
             </View>
         )
@@ -99,35 +127,72 @@ export default class Memo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            undo: []
+            undo: [],
+            done: []
         }
         this.undo = [];
+        this.done = [];
+    }
+
+    componentDidMount() {
+        let me = this;
+        AsyncStorage.getItem('@undo', (err, undo)=>{
+            if(err === null) {
+                undo = JSON.parse(undo) || [];
+                me.undo = _.assign([], undo);
+                me.setState({undo});
+            }
+        });
+        AsyncStorage.getItem('@done', (err, done)=>{
+            if(err === null) {
+                done = JSON.parse(done) || [];
+                me.done = _.assign([], done);
+                me.setState({done})
+            }
+        });
+
     }
 
     render() {
         return (
             <ViewContainer style={memoStyle.container}>
-                <Enter onAddEvent={(text) => {
-                    this.undo.push({text: text, time: new Date()});
-                    this.setState({undo: this.undo});
-                }}/>
+                <Enter
+                    onAddEvent={(text) => {
+                        //新增undo
+                        this.undo.push({text: text, time: new Date()});
+                        AsyncStorage.setItem('@undo', JSON.stringify(this.undo));
+                        this.setState({undo: this.undo});
+                    }}
+                />
+
+                <Done
+                    dataSource={this.state.done}
+                />
 
                 <Undo
                     dataSource={this.state.undo}
                     onFinish={(index)=>{
-                        this.undo.splice(index, 1);
-                        this.setState({undo: this.undo});
+                        let done = this.undo.splice(index, 1);
+                        //新增done
+                        this.done.push(done[0]);
+                        AsyncStorage.setItem('@done', JSON.stringify(this.done));
+                        this.setState({undo: this.undo, done: this.done});
                     }}
                     onDelete={(index)=>{
+                        //删除undo
                         this.undo.splice(index, 1);
+                        AsyncStorage.setItem('@undo', JSON.stringify(this.undo));
                         this.setState({undo: this.undo});
                     }}
-                    />
+                />
             </ViewContainer>
         )
     }
 }
 
+const doneStyle = StyleSheet.create({
+
+});
 
 const undoStyle = StyleSheet.create({
     container: {
